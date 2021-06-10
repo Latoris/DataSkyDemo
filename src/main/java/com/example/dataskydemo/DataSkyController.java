7e5a0f18b510a5f95009e01b4737571a03bfcb0b
Latoris <renjietang009649@gmail.com>
2021-06-10 21:33:37 +0800

    init

--- src/main/java/com/example/dataskydemo/DataSkyController.java
+++ src/main/java/com/example/dataskydemo/DataSkyController.java
@@ -3,12 +3,15 @@ package com.example.dataskydemo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class DataSkyController{
 
     @RequestMapping("/dataSkyDemo")
-    public void dataSkyProcess(@RequestBody DataSkyData skyProcess){
+    @ResponseBody
+    public String dataSkyProcess(@RequestBody DataSkyData skyProcess){
         System.out.println(skyProcess);
+        return "ok";
     }
 }
